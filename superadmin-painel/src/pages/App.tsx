825e8ad8f0e7f558d0a15aa25ce1df3c18dddd89
vicschweiger <134645175+vicschweiger@users.noreply.github.com>
2026-07-15 19:31:02 -0300

    fix: routes

--- superadmin-painel/src/pages/App.tsx
+++ superadmin-painel/src/pages/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import SuperAdminCompanies from '../pages/SuperAdminCompanies.tsx';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import SuperAdminCompanies from '../pages/SuperAdminCompanies';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/superadmin/companies" replace />} />
-      <Route path="/superadmin/companies" element={<SuperAdminCompanies />} />
-      <Route path="/dashboard" element={<SuperAdminCompanies />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/superadmin/companies" replace />} />
+        <Route path="/superadmin/companies" element={<SuperAdminCompanies />} />
+        <Route path="/dashboard" element={<SuperAdminCompanies />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
